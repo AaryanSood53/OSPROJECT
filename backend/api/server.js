286ea41d9d65f0f6d0b89f75369202f0ecacfec1
git config --global user.name Akhand0ps <akhandps08@gmail.com>
2025-03-26 23:24:54 +0530

    modified server.js

--- backend/api/server.js
+++ backend/api/server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const cors = require("cors")
 const path = require("path")
 
-const app = express()
+const app = express();
 app.use(express.json())
 app.use(cors())
 app.use(express.static(path.join(__dirname, "public")))
@@ -147,9 +147,5 @@ app.post("/bankers", (req, res) => {
 })
 
 // Start server
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-  console.log(`Open http://localhost:${PORT} in your browser`)
-})
+module.exports = app;
 
